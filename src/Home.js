2d9798535babaa1b3ee61a8a7443626a184fe1d4
Robert Kottelin <robert.kottelin@gmail.com>
2022-08-20 16:59:52 +0200

    first2

---
+++ src/Home.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+
+function Home() {
+    return (
+        <h1>This is the homepage</h1>
+    );
+}
+
+export default Home;
---
+++ src/Myvaccines.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+  
+const Myvaccines = () => {
+
+  return (
+    <div>
+      <p>This is the myvaccine page</p>
+    </div>
+  );
+};
+  
+export default Myvaccines;
